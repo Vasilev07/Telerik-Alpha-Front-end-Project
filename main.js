2e27650908beca58c9844cbde0a3db6c59673432
Vasilev07 <georgi_vasilev07@abv.bg>
2018-01-26 16:22:11 +0200

    import some javascript/jquery logic to add new button with Rusi

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 // get values from input filed and push it to array
 // save changes button get value from input field
-$('#save-changes').on('click', function(){
-    var newTask = $('#comment').val();
-    console.log('aaa');
+
+var arr = [];
+// attach event to add button on it's click
+$('#save-new-task').on('click', function () { // save changes
+    // take value from the input
+    var taskValue = $('#new-task-value').val().toString();
+    // push the task value to P item
+    for (var text of $(".create-new-task")) {
+        $(text).addClass('new-task-text-from-value');
+        $(text).removeClass('create-new-task');
+        $(text).text(taskValue);
+        arr.push(taskValue);
+        break
+    }
+    // delete input field (clear it)
+    $('#new-task-value').val('');
 });
+
+console.log(arr);
+
+// TODO
+// if close is clicked remove the value of input field
