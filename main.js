b519476b45c187cfc2895051f8ab5a3d14d5f7e3
Vasilev07 <georgi_vasilev07@abv.bg>
2018-01-29 14:14:57 +0200

    added datepicker with some logic aftec click on "save-changes"

--- main.js
+++ main.js
@@ -6,12 +6,18 @@ $('#save-new-task').on('click', function () { // save changes
     // take value from the input
     var taskValue = $('#new-task-value').val().toString();
     // push the task value to P item
-    $(".create-new-task").first()
+    $(".create-new-task")
+        .first()
         .addClass('new-task-text-from-value')
         .removeClass('create-new-task')
         .text(taskValue);
+
     // delete input field (clear it)
     $('#new-task-value').val('');
+    // get current date and append it to html
+    var currentDate = $('#datetimepicker1').data("DateTimePicker").useCurrent('year', 'month', 'day').date()._i.toString();
+    $(".current-date").first().removeClass('current-date').html(currentDate);
+    
 });
 
 // TODO
