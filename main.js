e7bb53186e4b97fe708849ba53475c254f5a4a8f
Vasilev07 <georgi_vasilev07@abv.bg>
2018-01-29 10:46:26 +0200

    changed some logic

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 // get values from input filed and push it to array
 // save changes button get value from input field
 
-var arr = [];
 // attach event to add button on it's click
 $('#save-new-task').on('click', function () { // save changes
     // take value from the input
@@ -11,12 +10,9 @@ $('#save-new-task').on('click', function () { // save changes
         .addClass('new-task-text-from-value')
         .removeClass('create-new-task')
         .text(taskValue);
-    arr.push(taskValue);
     // delete input field (clear it)
     $('#new-task-value').val('');
 });
-
-console.log(arr);
 
 // TODO
 // if close is clicked remove the value of input field
