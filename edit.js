a2e34250d5d407836bfa2c788795ad20cab7961f
Simeon  Ivanov <simeonivanov@yahoo.com>
2018-01-31 07:46:49 +0200

    Fixed bug in edit.js

--- edit.js
+++ edit.js
@@ -1,12 +1,13 @@
 
 // Option is to put only $(function(){}). This allows us to put this script befor the body.
 $(function () {    
-    $(".edit").on("click", function () {
+    $(".edit").click(function () {
         var rowId = $(this).parent().siblings(".check").children("p").attr("id");
         rowId = "#" + rowId;
         $("#save-edit-task").on("click", function () {
             var taskVal = $("#edit-task-value").val().toString();
             $(rowId).html(taskVal);
+            $('#new-task-value').val('');
         });
     });
 });
